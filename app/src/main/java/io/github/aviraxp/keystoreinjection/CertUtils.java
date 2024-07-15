06d3e71ddd09ef9837b88bce0120626387b2fa61
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2024-07-15 11:21:33 +0800

    Fix regex for parsing PEMs
    Lesson: never trust stackoverflow.

--- app/src/main/java/io/github/aviraxp/keystoreinjection/CertUtils.java
+++ app/src/main/java/io/github/aviraxp/keystoreinjection/CertUtils.java
@@ -14,11 +14,13 @@ import java.io.StringReader;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
+import java.util.regex.Pattern;
 
 public class CertUtils {
+    private static final Pattern lineTrimmer = Pattern.compile("^\\s+|\\s+$", Pattern.MULTILINE);
 
     public static Certificate parseCert(String cert) throws Throwable {
-        cert = cert.trim().replaceAll("(?m)^[\s&&[^\n]]+|[\s+&&[^\n]]+$", "");
+        cert = lineTrimmer.matcher(cert).replaceAll("");
         PemObject pemObject;
         try (PemReader reader = new PemReader(new StringReader(cert))) {
             pemObject = reader.readPemObject();
@@ -30,7 +32,7 @@ public class CertUtils {
     }
 
     public static X500Name parseCertSubject(String cert) throws Throwable {
-        cert = cert.trim().replaceAll("(?m)^[\s&&[^\n]]+|[\s+&&[^\n]]+$", "");
+        cert = lineTrimmer.matcher(cert).replaceAll("");
         PemObject pemObject;
         try (PemReader reader = new PemReader(new StringReader(cert))) {
             pemObject = reader.readPemObject();
@@ -42,7 +44,7 @@ public class CertUtils {
     }
 
     public static KeyPair parseKeyPair(String key) throws Throwable {
-        key = key.trim().replaceAll("(?m)^[\s&&[^\n]]+|[\s+&&[^\n]]+$", "");
+        key = lineTrimmer.matcher(key).replaceAll("");
         Object object;
         try (PEMParser parser = new PEMParser(new StringReader(key))) {
             object = parser.readObject();
@@ -54,7 +56,7 @@ public class CertUtils {
     }
 
     public static PrivateKey parsePrivateKey(String keyPair) throws RuntimeException {
-        keyPair = keyPair.trim().replaceAll("(?m)^[\s&&[^\n]]+|[\s+&&[^\n]]+$", "");
+        keyPair = lineTrimmer.matcher(keyPair).replaceAll("");
         try (PEMParser parser = new PEMParser(new StringReader(keyPair))) {
             PEMKeyPair pemKeyPair = (PEMKeyPair) parser.readObject();
             return new JcaPEMKeyConverter().getPrivateKey(pemKeyPair.getPrivateKeyInfo());
